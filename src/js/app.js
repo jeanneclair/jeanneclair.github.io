ab4e9dd674a9cabfa086ba25772e49ffa12bfa7a
Jeanne Clair <jeanne.m.clair@gmail.com>
2018-06-24 16:06:20 -0400

    vh of typewriter

--- src/js/app.js
+++ src/js/app.js
@@ -32,33 +32,3 @@ function typeWriter() {
   }
 }
 window.onload = typeWriter();
-
-
-
-// var i1 = 0;
-// var txt1 = '#educator #lifeLongLearner'; 
-// var speed1 = 50; 
-
-// function typeWriter1() {
-//   if (i1 < txt1.length) {
-//     document.getElementById("typed1").innerHTML += txt1.charAt(i1);
-//     i1++;
-//     setTimeout(typeWriter1, speed1);
-//   }
-// }
-// window.onload = typeWriter1();
-
-// var i2 = 0;
-// var txt2 = '#clarinetist #traveler'; 
-// var speed2 = 50; 
-
-// function typeWriter2() {
-//   if (i2 < txt2.length) {
-//     document.getElementById("typed2").innerHTML += txt2.charAt(i2);
-//     i2++;
-//     setTimeout(typeWriter2, speed2);
-//   }
-// }
-// window.onload = typeWriter2();
-
-
